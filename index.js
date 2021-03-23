517027632f610a0363124aa1c57acffd965deefc
Umutonipamera <umutonipamera@gmail.com>
2021-03-23 21:06:42 +0300

    Assignment 5 Js

---
+++ index.js
@@ -0,0 +1,18 @@
+function grades(marks){
+    if (marks>=80){
+        return"A";
+    }
+    else if(marks>=70){
+        return"B";
+    }
+    else if(marks>=60){
+        return"C";
+    }
+    else if(marks>=50){
+        return"D";
+    }
+    else if(marks>=0){
+        return"E";
+    }
+}
+console.log(grades(84));
